9aba8ba0153ac68eb34488a7561c649cb874ebd4
ericmay33 <ericbrianmay@optonline.net>
2025-02-19 10:15:34 -0500

    Message in byte length and byte array format

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        String mes = Message.getCurrentFileListing();
-        Message message = new Message("1.0", "124.0.0.10", "1234567890", mes);
+        String fileListString = Message.getCurrentFileListing();
         System.out.println();
-        System.out.println(message.getFileListing() + "\n");
-        System.out.println(message.getEncodedMessage());
+        System.out.println(fileListString + "\n");
+
+        Message message = new Message((byte) 1, "255.0.0.1", 12345, fileListString);
+
+        byte[] encodedMessage = message.getMessageBytes();
+
+        Message decodedMessage = Message.decode(encodedMessage);
+
+        System.out.println("Version: " + message.getVersion());
+        System.out.println("Node IP: " + message.getNodeIP());
+        System.out.println("Timestamp: " + message.getTimestamp());
+        System.out.println("File Listing: " + message.getFileListing() + "\n");
+
+        if (fileListString.equals(decodedMessage.getFileListing())) {
+            System.out.println("Test Passed: File listing matches!");
+        } else {
+            System.out.println("Test Failed: File listing mismatch.");
+        }
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.security.SecureRandom;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Node {
+
+    private String nodeIP;
+    private ArrayList<String> knownNodes;
+    private HashMap<String, Integer> lastHeartbeats;
+    private SecureRandom random;
+    private Timer timer;
+    private HashMap<String, ArrayList<String>> nodeFileLists;
+
+    public Node(String nodeIP) {
+        this.nodeIP = nodeIP;
+        this.knownNodes = new ArrayList<>();
+        this.lastHeartbeats = new HashMap<>();
+        this.random = new SecureRandom();
+        this.timer = new Timer();
+        this.nodeFileLists = new HashMap<>();
+    }
+
+    // knownNodes is a list of IP addresses of other nodes in the network
+    public void loadKnownNodes(ArrayList<String> knownNodes) {
+        this.knownNodes = knownNodes;
+        this.knownNodes.remove(this.nodeIP);
+    }
+
+    public void startHeartbeatTimer() {
+        int delay = random.nextInt(31) * 1000;
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                sendHeartbeat();
+            }
+        };
+        
+        timer.schedule(task, delay);
+    }
+
+    private void sendHeartbeat() {
+        // Sends a heartbeat message to all known nodes
+    }
+
+    private void receiveMessage(String message) {
+        // Receives a message from another node
+    }
+
+    private void checkNodeHealth() {
+        // Checks if any nodes have not sent a heartbeat in the last 30 seconds
+    }
+
+    private void updateNodeFileLists() {
+        // Updates the file lists of all known nodes
+    }
+
+    public ArrayList<String> getKnownNodes() {
+        return knownNodes;
+    }
+}
