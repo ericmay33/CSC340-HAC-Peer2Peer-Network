5546fa3f34fed9aead6830934bfef1f44d0d5f3d
ericmay33 <ericbrianmay@optonline.net>
2025-03-01 18:11:18 -0500

    Test update

--- src/P2PNode.java
+++ src/P2PNode.java
@@ -82,7 +82,7 @@ public class P2PNode {
         }
     
         this.nodeIP = myIP;
-        knownNodes.add(new Node("127.0.0.1", 7010)); // Send to ourselves for testing
+        knownNodes.add(new Node("127.0.0.1", 7000)); // Send to ourselves for testing
     }
 
     // Start sending heartbeats at random intervals
